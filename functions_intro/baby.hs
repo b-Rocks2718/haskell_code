cce89d488c3c7107c6b642ed502af86e915d14f2
Brooks Bryant <brooks.bryant@icloud.com>
2022-07-08 17:39:56 -0500

    added comments

--- functions_intro/baby.hs
+++ functions_intro/baby.hs
@@ -1,5 +1,10 @@
+-- function definition example
 doubleMe x = x + x
+
+-- functions can call other functions
 doubleUs x y = doubleMe x + doubleMe y
+
+-- 'if then else' example
 doubleSmallNumber x = if x > 100
-  then x
-  else x * 2
+                      then x
+                      else x * 2
